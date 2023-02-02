61515e73eb3abb9e6aaa1061223dbfb22727fb60
Mateus <mateuscristofori@gmail.com>
2023-02-02 16:55:52 -0300

    Create schema and method createUser

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,11 +1,27 @@
-import { Resolver, Query } from 'type-graphql';
+import { Resolver, Query, Mutation, Arg } from 'type-graphql';
+import { User } from '../schema/User';
+import { randomUUID } from 'crypto';
 
 @Resolver()
 export class UserResolver {
+  private database: User[] = [];
 
-  @Query(() => String)
-  async hello() {
-    return 'Testando novamente';
+
+  @Query(() => [User])
+  async getAllUsers() {
+    return this.database;
   }
 
+
+  @Mutation(() => User)
+  async createUser(@Arg('name') name: string) {
+    const user = {
+      id: randomUUID(),
+      name
+    }
+    
+    this.database.push(user);
+
+    return user;
+  }
 }
---
+++ backend/src/schema/User.ts
@@ -0,0 +1,11 @@
+import { ObjectType, Field, ID } from 'type-graphql';
+
+@ObjectType()
+export class User {
+
+  @Field(_type => ID)
+  id: string
+
+  @Field()
+  name: string
+}
